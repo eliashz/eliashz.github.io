6265ec37eb2f2e70c22f1d0b9424244a5fdfeb35
eliashz <eliashz@gmail.com>
2022-11-24 10:38:18 +0100

    start eng

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+// Footer Year
+
 const year = document.querySelector("#current-year");
 
 year.innerHTML = new Date().getFullYear();
+
+// Language versions
+
+const ENG = ['contact']
+
+const language = localStorage.getItem('language');
+console.log(language)
