99fc9e189dc309b6fb9195f068f0733e8a343498
eliashz <eliashz@gmail.com>
2022-11-24 11:59:32 +0100

    eng done

--- js/index.js
+++ js/index.js
@@ -8,27 +8,28 @@ year.innerHTML = new Date().getFullYear();
 
 // Language versions
 
-console.log(texts)
-
-const language = localStorage.getItem('language');
-
+const languageLS = localStorage.getItem('language');
 const engSpaMenu = document.getElementById('language');
 const engSpaText = document.querySelectorAll('.language');
 const engSpaA = document.getElementById('cv');
-console.log(engSpaText.length)
-console.log(engSpaText[0])
 
-if(language || language === 'SPA') {
-    engSpaMenu.textContent = 'ENG';
-    engSpaA.setAttribute('href', "/files/RS-ehz.pdf");
-    for (let i = 0; i < engSpaText.length; i++) {
-        engSpaText[i].textContent = texts.eng[i]        
+const changeLanguage = (language) => {
+    if(language === 'eng') {
+        engSpaMenu.textContent = 'spa';
+        engSpaA.setAttribute('href', "/files/RS-ehz.pdf");
+        for (let i = 0; i < engSpaText.length; i++) {
+            engSpaText[i].textContent = texts.eng[i]        
+        }
+        localStorage.setItem('language', 'eng');
+    } else if (language === 'spa'){
+        engSpaMenu.textContent = 'eng';
+        engSpaA.setAttribute('href', "/files/CV-ehz.pdf");
+        for (let i = 0; i < engSpaText.length; i++) {
+            engSpaText[i].textContent = texts.spa[i]        
+        }
+        localStorage.setItem('language', 'spa');
     }
-} else {
-    engSpaMenu.textContent = 'SPA';
-    engSpaA.setAttribute('href', "/files/CV-ehz.pdf");
-    for (let i = 0; i < engSpaText.length; i++) {
-        engSpaText[i].textContent = texts.spa[i]        
-    }
-
 }
+changeLanguage(languageLS || 'eng');
+
+engSpaMenu.addEventListener('click', () => changeLanguage(engSpaMenu.textContent))
