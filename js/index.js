5669981c680b95134f4f70b4a0420f13993ce584
eliashz <eliashz@gmail.com>
2022-11-24 10:50:18 +0100

    adding eng

--- js/index.js
+++ js/index.js
@@ -6,7 +6,19 @@ year.innerHTML = new Date().getFullYear();
 
 // Language versions
 
-const ENG = ['contact']
+const ENG = ['contact', ]
 
 const language = localStorage.getItem('language');
-console.log(language)
+
+const engSpaMenu = document.getElementById('language');
+const engSpaText = document.querySelectorAll('.language')
+
+console.log(engSpaText[0])
+
+if(!language || language === 'SPA') {
+    engSpaMenu.textContent = 'ENG';
+    engSpaText[0].textContent = ENG[0]
+} else {
+    engSpaMenu.textContent = 'SPA';
+
+}
